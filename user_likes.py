8e5fc65713d9e4d35b6d1fe295b61238dde6c600
chase coogan <chasecoogan12@gmail.com>
2022-11-12 17:46:18 -0500

    clean branch

--- user_likes.py
+++ user_likes.py
@@ -5,7 +5,6 @@ Function to create nested dictionary for storing and returning
 the like/disliked items
 """
 
-
 def check_like(category, like):
     get_like = {"like": {category}, "dislike":{category}}
 
