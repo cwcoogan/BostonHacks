6d9f1d854dae903e022cfaf286f48c31d8f7da2a
hams7504 <malli.hamsini@gmail.com>
2022-11-12 16:46:03 -0500

    made a calculator

--- food app/main.py
+++ food app/main.py
@@ -1,13 +1,69 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
 class MainApp(App):
     def build(self):
-        label = Label(text='Hello from Kivy',
-                      size_hint=(.5, .5),
-                      pos_hint={'center_x': .5, 'center_y': .5})
+        self.icon = "food.png"
+        self.operators = ["constructor elements go here"]
+        self.last_was_operator = None
+        self.last_button = None
+        main_layout = BoxLayout(orientation = "vertical")
+        self.solution = TextInput(background_color = "black", foreground_color = "white")
+        main_layout.add_widget(self.solution)
+        buttons = [
+            ["7", "8", "9", "/"],
+            ["4", "5", "6", "*"],
+            ["1", "2", "3", "+"],
+            [".", "0", "C", "-"],
+        ]
+        for row in buttons:
+            h_layout = BoxLayout()
+            for label in row:
+                button = Button(
+                    text = label, font_size = 30, background_color = "white",
+                    pos_hint = {"center_x": 0.5, "centery_y": 0.5}
+                )
+                button.bind(on_press=self.on_button_press)
+                h_layout.add_widget(button)
+            main_layout.add_widget(h_layout)
 
-        return label
+        equal_button = Button(
+                    text = "=", font_size = 30, background_color = "white",
+                    pos_hint = {"center_x": 0.5, "centery_y": 0.5}
+                )
+        equal_button.bind(on_press=self.on_solution)
+        main_layout.add_widget(equal_button)
+
+        return main_layout
+
+    def on_button_press(self, instance):
+        current = self.solution.text # our screen
+        button_text = instance.text
+
+        if button_text == "C":
+            self.solution.text = ""
+        else:
+            if current and (
+                self.last_was_operator and button_text in self.operators):
+                return
+            elif current == "" and button_text in self.operators:
+                return
+            else:
+                new_text = current + button_text
+                self.solution.text = new_text
+        self.last_button = button_text
+        self.last_was_operator = self.last_button in self.operators
+
+
+    def on_solution(self, instance):
+        text = self.solution.text
+        if text:
+            solution = str(eval(self.solution.text))
+            self.solution.text = solution
+
 
 if __name__ == '__main__':
     app = MainApp()
