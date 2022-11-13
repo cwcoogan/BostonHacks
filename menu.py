2662fa92d1570858010d5bcdfb4e5531eab5e128
chase coogan <chasecoogan12@gmail.com>
2022-11-12 22:40:20 -0500

    updated menu

--- menu.py
+++ menu.py
@@ -7,33 +7,33 @@
 
     Notes -- We will need to alter this from L/D/S to the actual physical swipe value 
 """
-preference = {}
+preference = {
+    'like':[],
+    'dislike':[],
+    'superlike':[]
+}
 
 def menu():
-    user_choice = input("Welcome to Food Tinder:\nL: Like \nD: Dislike\n"
-    "S: Super Like")   
-    if user_choice.upper() == "L":
-        return 1
-    elif user_choice.upper() == "D":
-        return 2
-    elif user_choice.upper() == "S":
-        return 3
+    i = 0
+    temp_list = ["Frozen","dairy", "water", "Bread", "cheese","Frozen","dairy"]
+    
+    for j in temp_list:
+        user_choice = input(f"Welcome to Food Tinder:\nL: Like \nD: Dislike\nS: Super Like\nFood choice is: {j}\n")   
+    
+        if user_choice.upper() == "L" and j not in preference['like']:
+            preference['like'].append(j)
 
-def check_like(category, like):
-    temp = [] # empty list to store item that will be removed from 
-    super_list = [] # list to store value of super_Liked item
-    
-    preference = {"like": [category], "dislike":[category]} # dictionary to fill  
-    if like == 1 and like not in  category: # if user selects LIKE & item is NOT in dict THEN append
-        preference['like'] = like
-    if like == 1 and like in category:
-        pass # needs to Continue 
-    if like == 2:
-        preference.pop(preference[like])
-        temp.append(category)
-    if like == 3:
-        super_list.append(like)
-    
+        elif user_choice.upper() == "D" and j not in preference['dislike']:
+            preference['dislike'].append(j)
+
+            if j in preference['like']:
+                preference['like'].remove(j)    
+
+        elif user_choice.upper() == "S":
+            preference['superlike'].append(j)
+        print(preference)
+
+
 menu()
-check_like('food', None)
+#print(preference)
     
