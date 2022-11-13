20ce2ee5cd6e1ae19064327025d5f79f474678d7
chase coogan <chasecoogan12@gmail.com>
2022-11-12 21:24:56 -0500

    waiting on neel to push

--- menu.py
+++ menu.py
@@ -1,9 +1,4 @@
-# globals for now, can change to local as needed.. 
-
-
-
-def menu(): 
-    """
+"""
     FUNCTION -- Menu()
 
     Parameter - Null
@@ -11,25 +6,29 @@ def menu():
     Returns -- user_choice int value of like/dislike/super-like
 
     Notes -- We will need to alter this from L/D/S to the actual physical swipe value 
-    """
+"""
+preference = {}
 
-    like = 1
-    dislike = 2
-    super_like = 3
-
+def menu():
     user_choice = input("Welcome to Food Tinder:\nL: Like \nD: Dislike\n"
-    "S: Super Like")     
+    "S: Super Like")   
     if user_choice.upper() == "L":
-        return like
+        return 1
     elif user_choice.upper() == "D":
-        return dislike
+        return 2
     elif user_choice.upper() == "S":
-        return super_like
-menu()
+        return 3
+
+def check_like(category, like):
+    empty = []
+    preference = ({"like": {category}, "dislike":{category}})   
+    if like == 1:
+        if category in preference[1]:
+            category.pop(1)
+            empty.append(category) 
+            preference['like'] = empty
+    
+
 
      
-
-
-
-
     
--- user_likes.py
+++ user_likes.py
@@ -1,4 +1,6 @@
-import api_test
+# import api_test
+from menu import *
+
 
 """
 Function to create nested dictionary for storing and returning 
@@ -6,13 +8,13 @@ the like/disliked items
 """
 
 def check_like(category, like):
-    get_like = {"like": {category}, "dislike":{category}}
+    get_like = {}
+    get_like.update({"like": {category}, "dislike":{category}})
+    if user_choice == 1:
 
 
     
-    
-    
-    
+
     # check if like/dislike
     # add to selected list
     # return both lists
